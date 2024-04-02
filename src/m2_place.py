4b40e2f52d2869030f97c2a95c60f5ef93cfd08a
maitetalavera <mtalavera001@student.franciscan.edu>
2024-04-02 12:06:44 -0400

    completed m3

--- src/m2_place.py
+++ src/m2_place.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   First, create a tkinter window called window.
 #
@@ -15,7 +15,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, create one frame dimensions 200 by 200.
 #
@@ -31,51 +31,18 @@ import tkinter as tk
 #
 ###############################################################################
 
-#window=tk.Tk()
+window=tk.Tk()
 
-#frame1=tk.Frame(master=window, width=100, height=100, bg="red")
-#frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+frame=tk.Frame(master=window, width=200, height=200)
+frame.pack()
 
-#frame2=tk.Frame(master=window, width=50, height=50, bg="yellow")
-#frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+label1=tk.Label(master=frame, text="Label A", bg="red")
+label1.place(x=0, y=0)
 
-#frame3=tk.Frame(master=window, width=25, height=25, bg="blue")
-#frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+label2=tk.Label(master=frame, text="Label B", bg="yellow")
+label2.place(x=65, y=75)
 
-#frame=tk.Frame(master=window, width=150, height=150)
-#frame.pack()
+label3=tk.Label(master=frame, text="Label C", bg="purple")
+label3.place(x=150, y=150)
 
-#label1=tk.Label(master=frame, text="I'm at (0,0)", bg="red")
-#label1.place(x=0, y=0)
-
-#label2=tk.Label(master=frame, text="I'm at (65,75)", bg="yellow")
-#label2.place(x=65, y=75)
-
-#for i in range(3):
-#    window.columnconfigure(i, weight=1, minsize=75)
-#    window.rowconfigure(i, weight=1, minsize=50)
-
-#    for j in range(3):
-#        frame=tk.Frame(
-#            master=window,
-#            relief=tk.RAISED,
-#            borderwidth=1
-#        )
-#        frame.grid(row=i, column=j, padx=5, pady=5)
-#        label=tk.Label(master=frame, text=f"Row {i}\nColumn{j}")
-#        label.pack()
-
-#window.rowconfigure(0,weight=1, minsize=50)
-#window.columnconfigure([0, 1, 2, 3], weight=1, minsize=50)
-
-#label1=tk.Label(text="1", bg="black", fg="white")
-#label2=tk.Label(text="2", bg="black", fg="white")
-#label3=tk.Label(text="3", bg="black", fg="white")
-#label4=tk.Label(text="4", bg="black", fg="white")
-
-#label1.grid(row=0, column=0)
-#label2.grid(row=0, column=1, sticky="ew")
-#label3.grid(row=0, column=2, sticky="ns")
-#label4.grid(row=0, column=3, sticky="nsew")
-
-#window.mainloop()
+window.mainloop()
